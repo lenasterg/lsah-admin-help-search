d6d04b1c12ae6eb76c2f167f1373f12e6830b487
Lena Stergatou <lenasterg@users.noreply.github.com>
2026-01-12 17:34:50 +0200

    Add license details to lsah-admin-help-search.php
    Add license information to the file header.

--- lsah-admin-help-search.php
+++ lsah-admin-help-search.php
@@ -7,7 +7,8 @@
  * Text Domain: lsah-admin-help-search
  * Domain Path: /languages
  * Network: true
- *
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * @package LSAH_Admin_Help_Search
  */
 
